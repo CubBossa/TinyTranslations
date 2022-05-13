5a2596bc4455ea9afbbde6a42b148abec8c2b1b2
CubBossa <leonard.bausenwein@web.de>
2022-05-13 17:08:40 +0200

    Initial

---
+++ src/main/java/de/cubbossa/translations/Message.java
@@ -0,0 +1,58 @@
+package de.cubbossa.translations;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class Message implements ComponentLike {
+
+	@Getter
+	private final String key;
+
+	public @NotNull Component asComponent() {
+		return this.asComponent(TranslationHandler.getInstance().getAudiences().console());
+	}
+
+	public Component asComponent(Player player, TagResolver... templates) {
+		return TranslationHandler.getInstance().translateLine(this, player, templates);
+	}
+
+	public List<Component> asComponents(Player player, TagResolver... templates) {
+		return TranslationHandler.getInstance().translateLines(this, player, templates);
+	}
+
+	public Component asComponent(Audience audience, TagResolver... templates) {
+		return TranslationHandler.getInstance().translateLine(this, audience, templates);
+	}
+
+	public List<Component> asComponents(Audience audience, TagResolver... templates) {
+		return TranslationHandler.getInstance().translateLines(this, audience, templates);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Message message = (Message) o;
+
+		return key.equals(message.key);
+	}
+
+	@Override
+	public int hashCode() {
+		return key.hashCode();
+	}
+}
---
+++ src/main/java/de/cubbossa/translations/TranslationHandler.java
@@ -0,0 +1,193 @@
+package de.cubbossa.translations;
+
+import com.google.common.collect.Lists;
+import lombok.Getter;
+import lombok.Setter;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.identity.Identity;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.Context;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+
+@Getter
+public class TranslationHandler {
+
+	@Getter
+	private static TranslationHandler instance;
+
+	private final File languageDirectory;
+
+	private final JavaPlugin plugin;
+	private final BukkitAudiences audiences;
+	private final MiniMessage miniMessage;
+
+	private final List<TagResolver> globalReplacements;
+	private static final Map<String, Map<String, String>> languageFormats = new HashMap<>();
+
+	@Setter
+	private boolean useClientLanguage = false;
+	@Setter
+	private String fallbackLanguage = Locale.US.toString();
+	@Setter
+	private @Nullable String loadDirectory;
+
+	public TranslationHandler(JavaPlugin plugin, BukkitAudiences audiences, MiniMessage miniMessage, File directory, @Nullable String loadDirectory) {
+		instance = this;
+
+		this.plugin = plugin;
+		this.audiences = audiences;
+		this.miniMessage = miniMessage;
+		this.languageDirectory = directory;
+		this.globalReplacements = new ArrayList<>();
+		this.loadDirectory = loadDirectory;
+	}
+
+	private Tag insertMessage(ArgumentQueue argumentQueue, Context context, Audience audience) {
+		final String messageKey = argumentQueue.popOr("The message tag requires a message key, like <message:error.no_permission>.").value();
+		return Tag.inserting(new Message(messageKey).asComponent(audience));
+	}
+
+	public void loadLanguages(Locale... createItAbsent) throws IOException {
+
+		if (!languageDirectory.exists()) {
+			languageDirectory.mkdir();
+		}
+		File[] files = languageDirectory.listFiles();
+		if (files == null) {
+			throw new NullPointerException("Language directory was null: " + languageDirectory.getAbsolutePath());
+		}
+		List<File> filesToLoad = new ArrayList<>();
+		for (File file : files) {
+			if (!file.getName().endsWith(".yml")) {
+				continue;
+			}
+			filesToLoad.add(file);
+		}
+		for (Locale l : createItAbsent) {
+			filesToLoad.add(new File(plugin.getDataFolder(), (loadDirectory != null ? loadDirectory + (loadDirectory.endsWith("/") ? "" : "/") : "") + l.toString() + ".yml"));
+		}
+		filesToLoad.stream().distinct().forEach(file -> {
+			try {
+				loadLanguage(file);
+			} catch (IOException e) {
+				plugin.getLogger().log(Level.SEVERE, "Could not load File: " + file.getAbsolutePath(), e);
+			}
+		});
+	}
+
+	public void loadLanguage(final File languageFile) throws IOException {
+		File file = languageFile;
+		String languageKey = file.getName().replace(".yml", "");
+
+		String fileName = (loadDirectory != null ? loadDirectory + (loadDirectory.endsWith("/") ? "" : "/") : "") + file.getName();
+
+		if (!file.exists()) {
+			plugin.saveResource(fileName, false);
+			file = new File(plugin.getDataFolder(), fileName);
+			if (!file.exists()) {
+				throw new IOException("Could not create language file: " + fileName);
+			}
+		}
+		YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+		Map<String, Object> map = cfg.getValues(true);
+		for (Map.Entry<String, Object> entry : map.entrySet()) {
+			if (entry.getValue() instanceof String s) {
+
+				Map<String, String> translations = languageFormats.getOrDefault(entry.getKey(), new HashMap<>());
+				translations.put(languageKey, s);
+				languageFormats.put(entry.getKey(), translations);
+			}
+		}
+	}
+
+	public void registerTagResolver(TagResolver resolver) {
+		globalReplacements.add(resolver);
+	}
+
+	public void unregisterTagResolver(TagResolver tagResolver) {
+		globalReplacements.remove(tagResolver);
+	}
+
+	private String getMiniMessageFormat(Message message, String lang) {
+		return languageFormats.getOrDefault(message.getKey(), new HashMap<>()).getOrDefault(lang, "missing:" + lang + "-" + message.getKey());
+	}
+
+	public Component translateLine(Message message, Player player, TagResolver... tagResolvers) {
+		return translateLine(getMiniMessageFormat(message, getLanguage(player)), player, tagResolvers);
+	}
+
+	public Component translateLine(Message message, Audience audience, TagResolver... tagResolvers) {
+		return translateLine(getMiniMessageFormat(message, getLanguage(audience)), audience, tagResolvers);
+	}
+
+	public List<Component> translateLines(Message message, Player player, TagResolver... tagResolvers) {
+		return translateLines(getMiniMessageFormat(message, getLanguage(player)), player, tagResolvers);
+	}
+
+	public List<Component> translateLines(Message message, Audience audience, TagResolver... tagResolvers) {
+		return translateLines(getMiniMessageFormat(message, getLanguage(audience)), audience, tagResolvers);
+	}
+
+
+	public Component translateLine(String message, Player player, TagResolver... tagResolvers) {
+		return translateLine(message, audiences.player(player), tagResolvers);
+	}
+
+	public Component translateLine(String message, Audience audience, TagResolver... tagResolvers) {
+
+		List<TagResolver> t = Lists.newArrayList(tagResolvers);
+		t.addAll(globalReplacements);
+		t.add(TagResolver.builder().tag("message", (argumentQueue, context) -> insertMessage(argumentQueue, context, audience)).build());
+
+		return miniMessage.deserialize(message, t.toArray(TagResolver[]::new));
+	}
+
+	public List<Component> translateLines(String message, Player player, TagResolver... tagResolvers) {
+		return translateLines(message, audiences.player(player), tagResolvers);
+	}
+
+	public List<Component> translateLines(String message, Audience audience, TagResolver... tagResolvers) {
+
+		List<TagResolver> t = new ArrayList<>(List.of(tagResolvers));
+		t.addAll(globalReplacements);
+		t.add(TagResolver.builder().tag("message", (argumentQueue, context) -> insertMessage(argumentQueue, context, audience)).build());
+		TagResolver[] resolvers = t.toArray(TagResolver[]::new);
+
+		String[] toFormat = message.split("\n");
+		List<Component> result = Arrays.stream(toFormat).map(s -> miniMessage.deserialize(s, resolvers)).collect(Collectors.toList());
+
+		// If all lines are empty, return empty list. Allows to have an empty lore.
+		if (result.stream().allMatch(component -> component.equals(Component.text("")))) {
+			return new ArrayList<>();
+		}
+		return result;
+	}
+
+	public String getLanguage(Audience audience) {
+		if (useClientLanguage) {
+			return audience.getOrDefault(Identity.LOCALE, Locale.US).getLanguage();
+		}
+		return fallbackLanguage;
+	}
+
+	public String getLanguage(Player player) {
+		if (useClientLanguage) {
+			return player.getLocale();
+		}
+		return fallbackLanguage;
+	}
+}
